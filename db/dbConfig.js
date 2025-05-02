170c54232cc4d4640881045e8b3b9a17f7420546
christian orlando <christianorlando@pursuit.org>
2025-05-01 20:16:02 -0400

    dbconfig

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -1,16 +1,36 @@
 
 
-// local dev config
-const pgp = require("pg-promise")();
+const pgp = require('pg-promise')();
 require("dotenv").config();
 
-const cn = {
+const { DATABASE_URL } = process.env
+
+
+const cn =  DATABASE_URL ?
+ { connectionString: DATABASE_URL, max:30,}
+:{
     host: process.env.PG_HOST,
     port: process.env.PG_PORT,
     database: process.env.PG_DATABASE,
-    user: process.env.PG_USER
-};
+    user: process.env.PG_USER,
+}; 
 
 const db = pgp(cn);
 
 module.exports = db;
+
+
+// local dev config
+// const pgp = require("pg-promise")();
+// require("dotenv").config();
+
+// const cn = {
+//     host: process.env.PG_HOST,
+//     port: process.env.PG_PORT,
+//     database: process.env.PG_DATABASE,
+//     user: process.env.PG_USER
+// };
+
+// const db = pgp(cn);
+
+// module.exports = db;
