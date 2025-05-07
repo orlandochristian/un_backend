f149d15e621aec21ffb22268ee1ab130e1936fca
christian orlando <christianorlando@pursuit.org>
2025-05-07 19:12:51 -0400

    Gemini model changed

--- db/schema.sql
+++ db/schema.sql
@@ -6,17 +6,17 @@
 
 
 
---  CREATE TABLE bmci (
---     id SERIAL PRIMARY KEY,
---     book_name varchar(255),
---     chapter varchar(255),
---     titulo_articulo varchar(255) not null,
---     articulo text not null,
---     autor varchar(255) not null,
---     authorizedby varchar(255) not null,
---     area_id INTEGER NOT NULL REFERENCES areas (id)
+ CREATE TABLE bmci (
+    id SERIAL PRIMARY KEY,
+    book_name varchar(255),
+    chapter varchar(255),
+    titulo_articulo varchar(255) not null,
+    articulo text not null,
+    autor varchar(255) not null,
+    authorizedby varchar(255) not null,
+    area_id INTEGER NOT NULL REFERENCES areas (id)
 
---  );
+ );
 
 
 --  CREATE TABLE areas (
@@ -27,7 +27,7 @@
 --     visible boolean
 --  );
 
---  CREATE TABLE departments (
+--  CREATE TABLE diplomats (
 --     id SERIAL PRIMARY KEY,
 --     titular varchar(255),
 --     back_up varchar(255),
@@ -41,11 +41,11 @@
 --     title varchar(255),
 --     descripcion varchar(255),
 --     visible boolean,
---     department_id INTEGER NOT NULL REFERENCES departments (id)
+--     diplomat_id INTEGER NOT NULL REFERENCES diplomats (id)
 --  );
 
 -- Enable pgvector extension
--- CREATE EXTENSION IF NOT EXISTS vector;
+CREATE EXTENSION IF NOT EXISTS vector;
 
 -- Recreate the users table with the correct columns
 -- CREATE TABLE IF NOT EXISTS topics (
@@ -61,4 +61,4 @@
 -- ALTER TABLE users ADD COLUMN embedding vector(1536);
 
 --Updating the embedding column to store vectors (768 for GeminiAI)
-ALTER TABLE bmci ADD  embedding TYPE vector(768);
+ALTER TABLE bmci ADD  embedding vector(768);
--- db/seed.sql
+++ db/seed.sql
@@ -1,31 +1,54 @@
 
---  \c belgium;
+-- --  \c belgium;
 
+-- areas table ---------------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------------------------------
 
 -- insert into areas (areaname,descripcion,assistant,visible) values 
 -- ('Legal Advisor','Provide legal advice and services to the UN Secretariat and other UN organs. They also help develop international law',
 -- 'Majorlein Van DOOREN',TRUE);
 
--- insert into areas (areaname,descripcion,assistant,visible) values 
+-- insert into areas (areaname,descripcion,assistant,visible,comingsoon,endpoint,icon) values 
 --  ('Human Rights','Promotes and protects human rights through a variety of activities, including the Universal Declaration of Human Rights, the UN High Commissioner for Human Rights, and the Human Rights Council',
--- 'Majorlein Van Dooren',TRUE);
+-- 'Majorlein Van Dooren',TRUE,TRUE,'hr','LiaBalanceScaleRightSolid');
 
--- insert into areas (areaname,descripcion,assistant,visible) values
---  ('Press, Communication & Outreach','Primarily focuses on managing the organization public image by strategically communicating its work and key messages to the global media and public through press releases, media outreach, partnerships with civil society groups and public events',
---   'Majorlein Van DOOREN',TRUE); 
 
--- insert into areas (areaname,descripcion,assistant,visible) values 
+-- insert into areas (areaname,descripcion,assistant,visible,comingsoon,endpoint,icon) values 
 --  ('Peace & Security','Keep international peace and security by working to prevent conflicts, facilitate peaceful resolutions through diplomacy and mediation, deploy peacekeeping operations when necessary',
--- 'Florence LATINIS',TRUE);
+-- 'Florence Latinis',TRUE,TRUE,'ps','GiPeaceDove');
 
--- insert into areas (areaname,descripcion,assistant,visible) values 
+-- insert into areas (areaname,descripcion,assistant,visible,comingsoon,endpoint,icon) values 
 -- ('Sustainable development & Humanitarian Aid','The United Nations (UN) works on sustainable development and humanitarian aid in many ways, including setting goals, coordinating responses, and providing aid',
--- 'Kathelijne COOREMAN',TRUE);
+-- 'Kathelijne Cooreman',TRUE,TRUE,'sdhi','MdAddchart');
 
--- insert into areas (areaname,descripcion,assistant,visible) values 
--- ('UN Budget & Management/ Cyber Issues','Is responsible for the UN finances, including budgeting, policy, and financial regulations. Covers the costs of its programs and activities in areas like human rights, international justice, and peacekeepin',
--- 'Marjolein Van DOOREN',TRUE);
+-- insert into areas (areaname,descripcion,assistant,visible,comingsoon,endpoint,icon) values 
+-- ('UN Budget & Management/ Fifth Committee','Is responsible for the UN finances, including budgeting, policy, and financial regulations. Covers the costs of its programs and activities in areas like human rights, international justice, and peacekeepin',
+-- 'Marjolein Van Dooren',TRUE,FALSE,'bmci','LiaAccusoft');
 
--- insert into areas (areaname,descripcion,assistant,visible) values 
+-- insert into areas (areaname,descripcion,assistant,visible,comingsoon,endpoint,icon) values 
 -- ('Election Officer','IThe Assembly annually elects five non-permanent members of the Security Council for two-year terms, 18 members of the Economic and Social Council for three-year terms, and 14–18 members of the Human Rights Council for three-year terms',
--- 'Marjolein Van DOOREN',TRUE);
+-- 'Marjolein Van Dooren',TRUE,TRUE,'eo','SlDrawer');
+
+-- insert into areas (areaname,descripcion,assistant,visible,comingsoon,endpoint,icon) values
+--  ('Press, Communication & Outreach','Primarily focuses on managing the organization public image by strategically communicating its work and key messages to the global media and public through press releases, media outreach, partnerships with civil society groups and public events',
+--   'Majorlein Van Dooren',TRUE,TRUE,'pco','GiOldMicrophone'); 
+
+-- Diplomats table ---------------------------------------------------------------------------------------
+---------------------------------------------------------------------------------------------------------
+
+-- insert into diplomats (titular,back_up,descripcion,visible,area_id) values
+-- ('Basiel Bogaerts','Karl Lagatie','',TRUE,5);
+
+
+
+-- offices table ---------------------------------------------------------------------------------------
+---------------------------------------------------------------------------------------------------------
+
+-- insert into offices (title,descripcion,visible,diplomat_id) values
+-- ('5th Committe','',TRUE,1);
+
+-- insert into offices (title,descripcion,visible,diplomat_id) values
+-- ('UN Budget,Financial Questions,UN change Mgt','',TRUE,1);
+
+-- insert into offices (title,descripcion,visible,diplomat_id) values
+-- ('Cyber,Internet Governance,Digital Compact','',TRUE,1);
